2d601748f52860584853dc7d1bd9140f42f5c7a7
CUPZYY <richbanana1234@gmail.com>
2024-08-27 12:52:20 +0200

    kandidater backend

--- server.py
+++ server.py
@@ -1,9 +1,15 @@
 from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
+
+kandidater = json.load(open("kandidater.json", "r"))
+print(kandidater)
+
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('index.html', kandidater=kandidater)
+
 
 if __name__ == '__main__':
     app.run()
