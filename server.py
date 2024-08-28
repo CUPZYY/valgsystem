7e7ff4e9b0dd4811f67e20a2b856718a6b1a2cb3
CUPZYY <richbanana1234@gmail.com>
2024-08-28 09:03:09 +0200

    add voting
    /api/vote?id=eksempel&candidate=eksempelInt

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import json
 
 app = Flask(__name__)
@@ -7,9 +7,40 @@ app.config['TEMPLATES_AUTO_RELOAD'] = True
 kandidater = json.load(open("kandidater.json", "r"))
 print(kandidater)
 
+def submit_vote(klasse, candidate):
+    with open('stemmer.json', 'r+', encoding='utf-8') as f:
+        stemmer = json.load(f)
+        stemmer[klasse][str(candidate)] =+ 1
+        f.seek(0)
+        f.truncate()
+        json.dump(stemmer, f, ensure_ascii=False, indent=4)
+
 @app.route("/")
 def index():
     return render_template('index.html', kandidater=kandidater)
+
+@app.route("/api/vote/")
+def vote():
+    with open('id.json', 'r', encoding='utf-8') as f:
+        ids = json.load(f)
+    args = dict(request.args)
+    id = args.get("id")
+    candidate = args.get("candidate")
+    klasse = None
+    for klassenavn in ids:
+        if id in ids[klassenavn]:
+            klasse = klassenavn
+            ids[klasse].remove(id)
+    if not klasse:
+        return "error"
+    
+    if candidate:
+        submit_vote(klasse, candidate)
+    
+    with open('id.json', 'w', encoding='utf-8') as f:
+        json.dump(ids, f, ensure_ascii=False, indent=4)
+
+    return "jess"
 
 
 if __name__ == '__main__':
