156158bce232814f881c4843014520aa80746aed
CUPZYY <richbanana1234@gmail.com>
2024-08-29 09:39:27 +0200

    masse greier som jeg har glemt å committe

--- server.py
+++ server.py
@@ -15,38 +15,42 @@ def submit_vote(klasse, candidate):
         f.truncate()
         json.dump(stemmer, f, ensure_ascii=False, indent=4)
 
+def find_klasse(id, remove=False):
+    with open('id.json', 'r+', encoding='utf-8') as f:
+        ids = json.load(f)
+        klasse = None
+        for klassenavn in ids:
+            if id in ids[klassenavn]:
+                klasse = klassenavn
+                if remove:
+                    ids[klasse].remove(id)
+                    json.dump(ids, f, ensure_ascii=False, indent=4)
+    return klasse
+
 @app.route("/")
 def index():
     return render_template('index.html')
 
-@app.route("/kandidater")
+@app.route("/vote")
 def show_candidates():
-    klasse = request.args.get('klasse')
-    if klasse in kandidater:
-        return render_template('kandidater.html', kandidater=kandidater[klasse].values(), klasse=klasse)
+    id = request.args.get("id")
+    klasse = find_klasse(id)
+    if not klasse:
+        return render_template('invalid.html')
     else:
         return render_template('kandidater.html', kandidater=[], klasse=klasse)
 
 @app.route("/api/vote/")
 def vote():
-    with open('id.json', 'r', encoding='utf-8') as f:
-        ids = json.load(f)
     args = dict(request.args)
     id = args.get("id")
     candidate = args.get("candidate")
-    klasse = None
-    for klassenavn in ids:
-        if id in ids[klassenavn]:
-            klasse = klassenavn
-            ids[klasse].remove(id)
+    klasse = find_klasse(id, remove=True)
     if not klasse:
         return "error"
     
     if candidate:
         submit_vote(klasse, candidate)
-    
-    with open('id.json', 'w', encoding='utf-8') as f:
-        json.dump(ids, f, ensure_ascii=False, indent=4)
 
     return "jess"
 
